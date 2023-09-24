4a99099081e77c1bab8da509869a34b266ed27ad
Misato-Maesaka <r012mbmaesaka@gmail.com>
2023-09-25 00:54:10 +0900

    error

--- src/main/java/com/cat/crudapi/servise/CatServiceImpl.java
+++ src/main/java/com/cat/crudapi/servise/CatServiceImpl.java
@@ -30,3 +30,8 @@ public class CatServiceImpl implements CatServise {
     }
 
 }
+
+/*
+ *エラー文
+ *2023-09-25T00:08:10.190+09:00  WARN 26394 --- [           main] ConfigServletWebServerApplicationContext : Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'catServiceImpl' defined in file [/Users/maesakamisato/Desktop/RaiseTechproject/crudapi/build/classes/java/main/com/cat/crudapi/servise/CatServiceImpl.class]: Unsatisfied dependency expressed through constructor parameter 0: Error creating bean with name 'catMapper' defined in file [/Users/maesakamisato/Desktop/RaiseTechproject/crudapi/build/classes/java/main/com/cat/crudapi/repositry/CatMapper.class]: Cannot resolve reference to bean 'sqlSessionTemplate' while setting bean property 'sqlSessionTemplate'
+ */
